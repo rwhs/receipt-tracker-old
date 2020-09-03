09da94f8bea449f0929dfc3dc893b0c885dd992b
rwhs <ronald.so1996@gmail.com>
2020-09-02 21:36:08 -0400

    fix migration non-nullable values

--- backend/app.py
+++ backend/app.py
@@ -2,11 +2,14 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from config import POSTGRES_URL, POSTGRES_DATABASE, POSTGRES_USERNAME, POSTGRES_PASSWORD
 
+
 # Initialize app
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://{POSTGRES_USERNAME}:{POSTGRES_PASSWORD}@{POSTGRES_URL}/{POSTGRES_DATABASE}'
 db = SQLAlchemy(app)
 
+
+# Import and register blueprints
 from api.users import users
 
 app.register_blueprint(users)
