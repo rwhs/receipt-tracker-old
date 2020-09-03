a150af39af2abe57daacc8ca3060d1600dc7f661
rwhs <ronald.so1996@gmail.com>
2020-09-03 00:56:55 -0400

    testing user authentication

--- backend/api/users.py
+++ backend/api/users.py
@@ -1,17 +1,45 @@
 from flask import Blueprint, request, jsonify
 from app import db
 from models import User
+import re
 
 users = Blueprint('users', __name__)
 
 
-@users.route('/api/users', methods=['POST'])
+@users.route('/api/register', methods=['POST'])
 def register():
     user = request.get_json()
+    email = user['email']
+    password = user['password']
 
-    new_user = User(email=user['email'])
+    new_user = User(email=email, password=password)
 
-    db.session.add(new_user)
-    db.session.commit()
+    # Validate email format
+    if re.fullmatch(r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)", email) == None:
+        return jsonify({'response': 'Email is not valid!'})
 
-    return jsonify({'response': 'testing'})
+    # Catch duplicate registered emails
+    try:
+        db.session.add(new_user)
+        db.session.commit()
+    except:
+        db.session.rollback()
+        return jsonify({'response': 'Email is already registered!'})
+
+    return jsonify({'response': f'Successfully registered {email}'})
+
+
+# Testing login route
+@users.route('/api/login', methods=['POST'])
+def login_test():
+    user = request.get_json()
+
+    stored_user = User.query.filter_by(email=user['email']).first()
+
+    if stored_user is not None:
+        if stored_user.verify_password(user['password']):
+            return jsonify({'response': 'Authenticated'})
+        else:
+            return jsonify({'response': 'Invalid credentials!'})
+    else:
+        return jsonify({'response': 'User does not exist'})
--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,12 @@
 from flask import Flask
+from flask_bcrypt import Bcrypt
 from flask_sqlalchemy import SQLAlchemy
 from config import POSTGRES_URL, POSTGRES_DATABASE, POSTGRES_USERNAME, POSTGRES_PASSWORD
 
 
 # Initialize app
 app = Flask(__name__)
+bcrypt = Bcrypt(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://{POSTGRES_USERNAME}:{POSTGRES_PASSWORD}@{POSTGRES_URL}/{POSTGRES_DATABASE}'
 db = SQLAlchemy(app)
 
