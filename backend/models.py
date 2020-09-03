a60df92fd799ef6401de60235b5ae1d6a1e40536
rwhs <ronald.so1996@gmail.com>
2020-09-03 00:55:55 -0400

    update models, migrations for authentication

--- backend/models.py
+++ backend/models.py
@@ -1,13 +1,30 @@
-from app import db
+from app import db, bcrypt
 from datetime import datetime
 
 
 class User(db.Model):
 
+    # User authentication
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
+    password_hash = db.Column(db.String(120))
+
+    first_name = db.Column(db.String(120))
+    last_name = db.Column(db.String(120))
     date_created = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     receipts = db.relationship('Receipt', backref='user', lazy=True)
+
+    @property
+    def password(self):
+        raise AttributeError('Passwords cannot be accessed!')
+
+    @password.setter
+    def password(self, password):
+        self.password_hash = bcrypt.generate_password_hash(password).decode('utf-8')
+
+    def verify_password(self, password):
+        return bcrypt.check_password_hash(self.password_hash, password)
+
 
 
 class Receipt(db.Model):
